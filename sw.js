52f4be9edabf94e4533f9fd3248a62c7f0c4f078
ICU <dop3boiz@gmail.com>
2026-03-12 18:42:37 +0000

    Use Rubik Glitch Pop font for title, bigger size

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "promptvault-v14";
+var CACHE = "promptvault-v15";
 var ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e) {
