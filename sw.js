f7f4d7c6cdefbcf42bd6f026bf067e4da7660fa9
ICU <dop3boiz@gmail.com>
2026-03-12 18:10:03 +0000

    Remove level and rating systems

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "promptvault-v10";
+var CACHE = "promptvault-v11";
 var ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e) {
