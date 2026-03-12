285e643360017a8bda692d9f542021eec1a56a73
ICU <dop3boiz@gmail.com>
2026-03-12 18:03:05 +0000

    Add 100 story/comedy prompts (1700 total)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "promptvault-v9";
+var CACHE = "promptvault-v10";
 var ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e) {
